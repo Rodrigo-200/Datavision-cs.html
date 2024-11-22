d79177aae0d2230b4cab0b0f154f73ed1f2696d7
Oleksandr Bubnov <bubnov1092006@gmail.com>
2024-11-22 09:09:19 +0000

    Stars changes

--- Datavision/Generic.cs
+++ Datavision/Generic.cs
@@ -7,10 +7,10 @@ namespace Datavision
         public static List<User> ListOfUsers = new List<User>() {
             new User() { Name="admin",Email="admin@gmail.com",Password="1234"},
             };
+
         public static User CurrentUser = new User();
         public static List<Comment> ListOfComments = new List<Comment>();
-        public static List<Comment> reviews = new List<Comment>();
+        public static List<Review> reviews = new List<Review>();
         public static List<Contact> contacts = new List<Contact>();
-
     }
 }
--- Datavision/Review.cs
+++ Datavision/Review.cs
@@ -1,20 +1,22 @@
 ﻿namespace Datavision
 {
-    public class Comment
+    public class Review
     {
         public string Username { get; set; }
         public string Email { get; set; }
         public string Content { get; set; }
+        public int Stars { get; set; }
 
-        public Comment()
+        public Review()
         {
         }
 
-        public Comment(string username, string email, string content)
+        public Review(string username, string email, string content, int Stars)
         {
             this.Username = username;
             this.Email = email;
             this.Content = content;
+            this.Stars = Stars;
         }
     }
 }
